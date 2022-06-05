6f34964258e502b2e83799d21010bda618021d59
unknown <inzamam.cu@gmail.com>
2022-06-05 20:29:47 +0800

    1st day

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-"# Striver-SDE-Sheet" 
+## C/C++ Visual Studio Code setup
+
+---
+
+## `This is an easy and handy C/C++ setup for Visual Studio Code.`
+
+- ## It works on both Windows and Linux.
+- ## To start at first install the C/C++ extension pack.
+- ## To compile and run press <kbd>ctrl</kbd> + <kbd>shift</kbd> + <kbd>B</kbd> key combination. And select compile and run.
+- ## To debug press <kbd>F5</kbd>.
+- ## To run without debugging press <kbd>ctrl</kbd> + <kbd>F5</kbd>.
---
+++ src/Striver SDE Sheet/setMatrixZero.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setZeros(vector<vector<int>> &matrix){
+    int rows = matrix.size(), cols = matrix[0].size();
+
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            if(matrix[i][j] == 0){
+                int ind = i - 1;
+                while (ind >= 0)
+                {
+                    if(matrix[ind][j] != 0){
+
+                        matrix[ind][j] = -1;
+                    }
+                    ind --;
+                }
+                ind = i + 1;
+                while (ind < rows)
+                {
+                   if(matrix[ind][j] != 0){
+
+                        matrix[ind][j] = -1;
+                    }
+                    ind ++;
+                }
+                ind = j - 1;
+                while (ind >= 0)
+                {
+                   if(matrix[i][ind] != 0){
+
+                    matrix[i][ind] = -1;
+                    }
+                    ind --;
+                }
+                ind = j + 1;
+                while (ind < cols)
+                {
+                   if(matrix[i][ind] != 0){
+
+                    matrix[i][ind] = -1;
+                    }
+                    ind ++;
+                }
+
+                
+            }
+        }
+    }
+
+    for(int i=0; i < matrix.size(); i++){
+        for(int j=0; j < matrix[0].size(); j++){
+             if(matrix[i][j] < 0){
+
+                    matrix[i][j] = 0;
+                    }
+        }
+    }
+}
+
+int main(int argc, char const *argv[]){
+vector<vector<int>> arr;
+arr = {{7, 19, 3},{4, 21, 0}};
+    
+    setZeros(arr);
+
+    for(int i=0; i < arr.size(); i++){
+        for(int j=0; j < arr[0].size(); j++){
+            cout << " " << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
