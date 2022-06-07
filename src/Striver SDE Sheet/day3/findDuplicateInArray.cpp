74e158b6ffba1b4a903bb4598d3e0fbdac4ba0c2
unknown <inzamam.cu@gmail.com>
2022-06-08 00:32:11 +0800

    day 1

--- src/Striver SDE Sheet/day3/findDuplicateInArray.cpp
+++ src/Striver SDE Sheet/day3/findDuplicateInArray.cpp
@@ -3,15 +3,18 @@ using namespace std;
 
 
 int findDuplicate(vector<int> &arr, int n){
-	int tempArray[n];
+    vector<int> tempArray(n);
+
     for (auto it: arr){
-        if(tempArray[it] < it){
-            tempArray[it] = it;
+        if(tempArray[it] < 0){
+            tempArray[it] = 1;
         }else{
             return it;
         }
     }
+
     return 0;
+
 }
 
 
